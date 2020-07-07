345dddcd12697a910a93f5532115afe9bd076ce4
Candice Tate <67024645+candicetate@users.noreply.github.com>
2020-07-07 17:45:05 -0600

    Fixed jump

--- javascript.js
+++ javascript.js
@@ -4,27 +4,42 @@ var character = document.getElementById("character");
 // Get block
 var block = document.getElementById("block");
 
+// Bottom Counter
+var counter = 0;
+
 // Jump
-function jump {
-    if(character.classList != "animate"){
-        character.classList.add("animate");
-    }
-    setTimeout(function(){character.classList.remove("animate");} ,500);
+function jump(event) {
+  if (character.classList == "animate") {
+    return;
+  }
+  character.classList.add("animate");
+  setTimeout(function () {
+    character.classList.remove("animate");
+  }, 300);
 }
+
+//Event Listener
+let jumpClick = document.querySelector("#character");
+jumpClick.addEventListener("click", jump);
+
+jump();
 
 // Check if you died
-var checkDead = setInterval(function(){
-    var characterTop =
-    parseInt(
-window.getComputedStyle(character).getPropertyValue("top"));
-var blockLeft =
-parseInt(
-window.getComputedStyle(block).getPropertyValue("left"));
+var checkDead = setInterval(function () {
+  var characterTop = parseInt(
+    window.getComputedStyle(character).getPropertyValue("top")
+  );
+  var blockLeft = parseInt(
+    window.getComputedStyle(block).getPropertyValue("left")
+  );
 
-if(blockLeft<20 && blockLeft>0 && characterTop>=130){
-    alert("You lose");
+  if (blockLeft < 20 && blockLeft > 0 && characterTop >= 130) {
     block.style.animation = "none";
-    block.style.display = "none";
-}
-
-},10);
+    alert(`Score: ` + Math.floor(counter / 100));
+    counter = 0;
+    block.style.animation = "block 1s infinite linear";
+  } else {
+    counter++;
+    document.getElementById("scoreSpan").innerHTML = Math.floor(counter / 100);
+  }
+}, 10);
